091d88ea41def3f36a89f3ece50271411ec39207
Manmohandash18 <manmohandash2008@gmail.com>
2026-08-21 19:03:46 +0530

    changed

---
+++ JS/Conditional_loops_operators.js
@@ -0,0 +1,103 @@
+//operator
+//Arithmetic
+
+// console.log(2+5);
+// console.log(2-5);
+// console.log(2*5);
+// console.log(5%3);
+// console.log(5**3);
+
+//Assignment Operator
+
+// let x=20;
+// let y=10;
+
+// x-=y;
+// console.log(x);
+
+// let x=20;
+// let y=10;
+// console.log(x==y);
+
+// 1. Null is loosely equal to undefined only
+// console.log(undefined==null);
+// console.log(undefined===null);
+// console.log(null==0);
+// console.log(null=="");
+// console.log(null==false);
+
+// >,<,>=,<=,(null->number);
+//null is equal to zero in number
+
+// console.log(null<=0);
+// console.log(null>=0);
+// console.log(null>0);
+// console.log(null<0);
+
+// console.log("Rohit">"Mohit");
+// console.log("R">"Mohit");
+
+// To compare 2 datatypes of different data type both of them first converted to number and then will be compared
+
+// console.log(10>="10");
+// console.log(null>="");
+// console.log(NaN==NaN);
+
+//for loop
+// for(let i=0; i<9;i++){
+//     console.log(i);
+// }
+
+//while loop
+// let i=0; 
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+//Do-while loop
+// let i=0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<10);
+
+//if else condition
+
+// let age=10;
+// if(age>18){
+//     console.log("Eligible for vote");
+// }else{
+//     console.log("Not eligible");
+// }
+
+//Logical operator
+// console.log(true&&true);
+// console.log(true&&false);
+// console.log(false||true);
+// console.log(false||false);
+
+// AND
+//If first value is false then return first . If first value is true return second
+// let a="Ma";
+// let b="Mab";
+// let c= a&& b;
+// console.log(c);
+
+// let a=10;
+// let b=20;
+// let c=a&&b;
+// console.log(c);
+
+//OR
+//If first is true then return first. else return second
+// let a=10;
+// let b=20;
+// let c=a||b;
+// console.log(c);
+
+// !=
+console.log(4!=5);
+console.log(5!=5);
+
+
---
+++ JS/Variable.js
@@ -0,0 +1,65 @@
+// let name="Manmohan";
+// console.log(name);
+// let age=20;
+// age=30;
+// console.log(age,name);
+
+//Number
+
+// const account=1234;
+// console.log(account);
+// let a=2.36;
+// console.log(a);
+
+//String
+
+// let str="Strike is coming";
+// console.log(str);
+
+//boolean
+
+// let login=true;
+// let f=false;
+// console.log(login,f);
+
+//undefined
+
+// let user;
+// console.log(user);
+
+//big int
+
+// let n=516363672783786778346723663n;
+// console.log(n);
+
+//null
+// let weather=null;
+// console.log(weather);
+
+//Primitive data type
+
+//symbol
+// const id1=Symbol("id");
+// const id2=Symbol("id");
+// console.log(id2==id1);
+
+//non-primitive data type
+//array,objects,functions
+//All non primitive data type are objects
+
+// let arr=[10,20,11,"Manmohan",true];
+// console.log(arr);
+
+//Manmohan 12312 18 gen
+
+// let user={
+    // name:"Manmohan",
+    // account:12312,
+    // age:18,
+    // category:'gen'
+// }
+let s=function add(){
+    console.log("Hello");
+}
+
+console.log(s);
